ef7d405df08ae28292353ac5f27ead32fcc6a2d9
Luke <lukasz.wajda888@gmail.com>
2023-03-31 00:49:49 +0200

    great change in MainNavigation

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { Route, Routes } from "react-router-dom";
 import Meetups from "./view/MeetUps";
 import Bests from "./view/Bests";
 import NewMeetup from "./view/NewMeetUp";
+import MainNavigation from "./layaout/MainNavigation";
 
 function App() {
   return (
     <div>
-      <h1>Witaj</h1>
+      <h1>Welcome</h1>
+      <MainNavigation />
       <Routes>
-        <Route path="/" element={<Meetups />} />
+        <Route path="/" element={<Meetups />} exact/>
         <Route path="/new-meetup" element={<NewMeetup />} />
         <Route path="/best" element={<Bests />} />
       </Routes>
--- src/layaout/MainNavigation.js
+++ src/layaout/MainNavigation.js
@@ -1,10 +1,21 @@
+import { Link } from 'react-router-dom';
+
+import moduleclasses from './MainNavigation.module.css';
 function MainNavigation() {
   return (
     <header>
       <div>Fun Meetup</div>
       <nav>
         <ul>
-          <li>Hej</li>
+          <li>
+            <Link to='/'>MeetUps</Link> {/*"use instead <a href"*/}
+          </li>
+          <li>
+            <Link to='/new-meetup'>Add New MeetUp</Link> 
+          </li>
+          <li>
+            <Link to='/best'>The Best</Link> 
+          </li>
         </ul>
       </nav>
     </header>
